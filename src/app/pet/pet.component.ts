a36ffe6b6f5bd7954b6afac1cb38c0531cd45d29
Chi-Cheong Weng <chicheongweng@gmail.com>
2024-06-02 10:09:56 +0800

    generate random pets

--- src/app/pet/pet.component.ts
+++ src/app/pet/pet.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PetService } from '../generated/api/pet.service';
 import { Pet } from '../generated/model/pet';
+import { Tag } from '../generated';
+import { Category } from '../generated';
 
 @Component({
   selector: 'app-pet',
@@ -9,11 +11,13 @@ import { Pet } from '../generated/model/pet';
 })
 export class PetComponent implements OnInit {
   pet: Pet | undefined;
+  CATEGORIES: Category[] = Array.from({length: 10}, (_, i) => ({ id: i + 1, name: `Category ${i + 1}` }));
 
   constructor(private apiService: PetService) { }
 
   ngOnInit() {
-    this.getPet();
+    for (var i = 0; i<10; i++)
+      this.addPet(this.generateRandomPet());
   }
 
   getPet(): void {
@@ -40,4 +44,28 @@ export class PetComponent implements OnInit {
       // You might want to update the UI here
     });
   }
+
+  generateRandomCategory(): Category {
+    const randomIndex = Math.floor(Math.random() * this.CATEGORIES.length);
+    return this.CATEGORIES[randomIndex];
+  }
+
+  generateRandomTag(): Tag {
+    return {
+      //id: Math.floor(Math.random() * 10), // Generate a random id
+      name: `Tag-${Math.floor(Math.random() * 10)}`, // Generate a random name
+    };
+  }
+
+  public generateRandomPet(): Pet {
+    return {
+      //id: Math.floor(Math.random() * 1000), // Generate a random id
+      name: `Pet-${Math.floor(Math.random() * 1000)}`, // Generate a random name
+      category: this.generateRandomCategory(),
+      photoUrls: Array.from({length: 3}, () => `https://example.com/photo-${Math.floor(Math.random() * 1000)}.jpg`),
+      tags: Array.from({length: 5}, () => this.generateRandomTag()), // Generate an array of 5 random tags
+      status: ['available', 'pending', 'sold'][Math.floor(Math.random() * 3)] as Pet.StatusEnum, // Randomly select a status and cast it to StatusEnum
+    };
+  }
+
 }
